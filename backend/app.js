31647aad58b87dc8aad7623c5f24d0ab210a7a20
Major <navikbipin@gmail.com>
2022-06-14 00:06:24 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const dotenv = require('dotenv');
+const path = require('path');
+const cors = require('cors');
+dotenv.config({path : './.env'})
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('tiny'))
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+module.exports = app;
